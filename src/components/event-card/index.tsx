c511bcac8f91a2a1dea9960bb2920171df30425e
Hesmondjeet Oon Chee Houng <hesmondjeet@Hesmondjeets-MacBook-Pro.local>
2024-08-30 16:06:31 +0800

    drafting project structure

---
+++ src/components/event-card/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './styles.module.scss'
+
+const EventCard = () => {
+  return (
+    <div className={styles.container}>EventCard</div>
+  )
+}
+
+export default EventCard
