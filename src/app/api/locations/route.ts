09d52d5949cf3089df090ef6e9daaae752f4aeb2
Hesmondjeet Oon Chee Houng <hesmondjeet@Hesmondjeets-MacBook-Pro.local>
2024-09-01 22:35:13 +0800

    add offset/limit

--- src/app/api/locations/route.ts
+++ src/app/api/locations/route.ts
@@ -1,9 +1,16 @@
+import { connectToMongoDB } from "@/lib/database";
 import Location from "@/models/location.model";
+import { NextApiRequest } from "next";
 import { NextResponse } from "next/server";
 
-export async function GET(request: Request) {
+export async function GET(request: NextApiRequest) {
+  connectToMongoDB();
+  const {searchParams} = new URL(request.url || "");
+  const offset = searchParams.get("offset") || 0;
+  const limit = searchParams.get("limit") || 10;
+  console.log(offset)
   try {
-    const locations = await Location.find();
+    const locations = await Location.find().skip(+offset).limit(+limit);
     return NextResponse.json({ status: "OK", data: locations });
   } catch (error) {
     return NextResponse.json({ status: "ERROR" }, { status: 500 });
