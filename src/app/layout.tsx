f2a0d2eff6384ecbd801b520815e97f596ff6d1e
jessicaleh <jessica.low@stickycareer.com>
2024-09-05 14:34:41 +0800

    CSS codes for review

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.scss";
 import { connectToMongoDB } from "@/lib/database";
+import SiteFooter from "@/components/site-footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,6 +20,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
+
+<SiteFooter/>
     </html>
   );
 }
--- src/components/site-footer/index.tsx
+++ src/components/site-footer/index.tsx
@@ -2,9 +2,22 @@ import React from 'react'
 import styles from './styles.module.scss'
 
 const SiteFooter = () => {
-  return (
-    <div className={styles.container}>SiteFooter</div>
+  return(
+    <><div className={styles.container}>
+
+      <div id="Disclaimer">
+       The information on this website is for the sole use of Toastmasters’ members, for Toastmasters business only. It is not to be used for solicitation and distribution of non-Toastmasters material or information.
+      </div>
+
+      <div id="Links">
+       <div>
+        <a href="./">Terms</a>
+        <a href="./">Privacy Policy</a>
+       </div>
+       <div>Website by <span>StickyCareer</span></div>
+      </div>
+      
+      </div></>
   )
 }
-
 export default SiteFooter
