ea790cfdc02b66916200987aff7a5c60beba8d63
Hesmondjeet Oon Chee Houng <hesmondjeet@Hesmondjeets-MacBook-Pro.local>
2024-09-07 00:14:02 +0800

    add components

---
+++ src/components/event-tags/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './styles.module.scss'
+
+const EventTags = () => {
+  return (
+    <div className={styles.container}>EventTags</div>
+  )
+}
+
+export default EventTags
---
+++ src/components/icon-button/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './styles.module.scss'
+
+const IconButton = () => {
+  return (
+    <div className={styles.container}>IconButton</div>
+  )
+}
+
+export default IconButton
