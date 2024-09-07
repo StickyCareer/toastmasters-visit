01f0a08974b03fadbc7317d900fc287112a4a80a
Hesmondjeet Oon Chee Houng <hesmondjeet@Hesmondjeets-MacBook-Pro.local>
2024-09-07 22:53:38 +0800

    sample for event card

--- src/components/event-card/index.tsx
+++ src/components/event-card/index.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import styles from './styles.module.scss'
 
-const EventCard = () => {
+interface EventCardProps {
+  event: any
+}
+
+const EventCard = ({event}: EventCardProps) => {
   return (
-    <div className={styles.container}>EventCard</div>
+    <div className={styles.container}>{event.name}</div>
   )
 }
 
